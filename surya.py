64a7bf39d06fc70638a69f7a6dddc32d431706a6
Suraj0023 <45939965+Surya0023@users.noreply.github.com>
2019-02-17 13:54:04 -0500

    Add files via upload

---
+++ surya.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+os.system('toilet -f bigascii14 "SURAJ" --filter gay')
+
+print("CODED :- SURAJ CHAVDA")
+print("Author:- SURAJ CHAVDA")
+print("_________@COPYRIGTED_________")
+
+print("FONTS ::-") 
+
+subprocess.call(["toilet","-f","ascii12"    ,raw_input('ascii12 text:-'),"--filter","gay"])
+subprocess.call(["toilet","-f","ascii9",raw_input('ascii9 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","bigascii12",raw_input('bigascii14 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","bigascii9",raw_input('bigascii9 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","bigmono12",raw_input('bigmono12 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","bigmono9",raw_input('bigmono9 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","circle",raw_input('circle text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","emboss",raw_input('emoboss text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","emboss2",raw_input('emboss2 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","future",raw_input('future text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","letter",raw_input('mono1 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","mono12",raw_input('mono2 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","mono9",raw_input('pagga text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","pagga",raw_input('smascii12 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","smascii12",raw_input('smascii9 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","smascii9",raw_input('smblock text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","smblock",raw_input('smbraille text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","smbraille",raw_input('smmono1 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","smmono12",raw_input('smmono2 text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","smmono9",raw_input('wideterm text :-'),"--filter","gay"])
+subprocess.call(["toilet","-f","wideterm",raw_input('smmono9 text :-'),"--filter","gay"])
+
+
+
+
+
+
+
+ 
+
